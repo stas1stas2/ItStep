f3885b9d9304d55c0202d937aea1675ab8968084
ss.savenko <ss.savenko@gmail.com>
2018-04-03 20:41:49 +0300

    Start Code

---
+++ StringWork/Program.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Text;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] articles = { "a", "an", "the" };
+            string[] alliance = { "and", "but", "for", "not", "so", "yet" };
+            string[] excuses = { "at", "by", "in", "of", "on", "or", "out", "to", "up" };
+            string[][] allWords = { articles, alliance, excuses };
+            string str = Console.ReadLine();
+            str = str.Trim();
+            str = RemoweDoubleSpaceFromString(str);
+            str = SymbolsSpacing(str);
+
+            str = ChangeFirstLiteral(str, allWords);
+            Console.WriteLine(str);
+        }
+
+        private static string RemoweDoubleSpaceFromString(string inputString)
+        {
+            var currentString = new StringBuilder();
+            currentString.Append(inputString);
+            int stringLength = currentString.Length;
+            for (int i = 1; i < stringLength; i++)
+            {
+                if (currentString[i] == ' ' && currentString[i - 1] == ' ')
+                {
+
+                    currentString.Remove(i, 1);
+
+                    i--;
+                }
+                stringLength = currentString.Length;
+            }
+            var returnValue = currentString.ToString();
+            return returnValue;
+        }
+        private static string SymbolsSpacing(string inputString)
+        {
+            var str = new StringBuilder();
+            str.Append(inputString);
+            int strLength = str.Length;
+            for (int i = 1; i < strLength; i++)
+            {
+                if ((str[i] == '.' || str[i] == ',' || str[i] == ':' || str[i] == ';' || str[i] == '!' || str[i] == '?') && str[i - 1] == ' ')
+                {
+                    if (i + 1 < strLength)
+                    {
+                        if (str[i + 1] == ' ')
+                        {
+                            str.Remove(i - 1, 1);
+                            strLength--;
+                        }
+                        else
+                        {
+                            char c = str[i];
+                            str[i] = ' ';
+                            str[i - 1] = c;
+
+                        }
+                    }
+                    else
+                    {
+                        str.Remove(i - 1, 1);
+                        strLength--;
+                    }
+                }
+
+                if (str[i] == '-')
+                {
+                    if (str[i - 1] != ' ')
+                    {
+                        str.Insert(i, ' ');
+                        strLength++;
+                        i++;
+                    }
+                    if (i + 1 < strLength)
+                        if (str[i + 1] != ' ')
+                        {
+                            str.Insert(i + 1, ' ');
+                            strLength++;
+                            i++;
+                        }
+                }
+
+            }
+            return str.ToString();
+        }
+        private static string ChangeFirstLiteral(string inputString, string[][] keyWords)
+        {
+            var str = new StringBuilder();
+            str.Append(inputString);
+            int strLength = str.Length;
+            bool IsIncludedInMass = false;
+            int firstSymb = -1;
+            StringBuilder word = new StringBuilder();
+            for (int i = 0, wordSize = 0; i < strLength; i++, wordSize++)
+            {
+
+                if (str[i] == ' ' || i + 1 == strLength)
+                {
+                    string stringWord = word.ToString();
+                    foreach (string keyword in keyWords[0])
+                    {
+                        if (stringWord.Equals(keyword))
+                        {
+                            IsIncludedInMass = true;
+                        }
+                    }
+                    foreach (string keyword in keyWords[1])
+                    {
+                        if (stringWord.Equals(keyword))
+                        {
+                            IsIncludedInMass = true;
+                        }
+                    }
+                    foreach (string keyword in keyWords[2])
+                    {
+                        if (stringWord.Equals(keyword))
+                        {
+                            IsIncludedInMass = true;
+                        }
+                    }
+                    if (IsIncludedInMass == true)
+                    {
+                        IsIncludedInMass = false;
+                        continue;
+                    }
+                    else
+                    {
+                        if (str[firstSymb] >= 97 && str[firstSymb] <= 122)
+                            str[firstSymb] = Char.ToUpper(str[firstSymb]);
+                    }
+                    firstSymb = -1;
+                    wordSize = -1;
+                    word.Clear();
+                    continue;
+                }
+                if (firstSymb == -1)
+                    firstSymb = i;
+                word.Insert(wordSize, str[i]);
+            }
+
+            return str.ToString();
+        }
+
+
+    }
+}
